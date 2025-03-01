1cdca25baa78fb8b70a756c0d94665e94dd68685
Bruno Tomé <ibrunotome@gmail.com>
2025-03-01 14:00:34 -0300

    refactor: re-configure shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,16 @@
-import tailwindcss from '@tailwindcss/vite'
 import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   modules: [
+    '@nuxtjs/tailwindcss',
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/icon',
+    'shadcn-nuxt',
   ],
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
   eslint: {
     config: {
       standalone: false,
