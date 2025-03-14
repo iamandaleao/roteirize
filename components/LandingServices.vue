647bc22840ed2dc43e56ea6a6103e1a0c1f90fde
Bruno Tomé <ibrunotome@gmail.com>
2025-03-14 17:31:50 -0300

    refactor: switch items position in services

--- components/LandingServices.vue
+++ components/LandingServices.vue
@@ -9,13 +9,13 @@
     </div>
 
     <div class="flex h-32 flex-col items-center justify-center gap-2 text-primary">
-      <Icon name="ph:clock" class="" size="48" />
-      <h4>Suporte rápido <br class="xl:hidden"> de verdade</h4>
+      <Icon name="ph:headset" class="" size="48" />
+      <h4>Atendimento <br class="xl:hidden"> especializado</h4>
     </div>
 
     <div class="flex h-32 flex-col items-center justify-center gap-2 text-primary">
-      <Icon name="ph:headset" class="" size="48" />
-      <h4>Atendimento <br class="xl:hidden"> especializado</h4>
+      <Icon name="ph:clock" class="" size="48" />
+      <h4>Entrega rápida <br class="xl:hidden"> de verdade</h4>
     </div>
 
     <div class="flex h-32 flex-col items-center justify-center gap-2 text-primary">
