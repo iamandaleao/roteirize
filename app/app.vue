a2c8fa918cf9032f111e151a7d0e89904897abee
Bruno Tomé <ibrunotome@gmail.com>
2025-03-20 19:01:38 -0300

    chore: move meta to nuxt config

--- app/app.vue
+++ app/app.vue
@@ -11,18 +11,6 @@ watch(page, () => {
   })
 })
 
-useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' },
-  ],
-  htmlAttrs: {
-    lang: 'pt-BR',
-  },
-})
-
 useSeoMeta({
   titleTemplate: `%s - ${seo?.siteName}`,
   ogSiteName: seo?.siteName,
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,12 @@ export default defineNuxtConfig({
       bodyAttrs: {
         class: 'antialiased',
       },
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+      ],
       link: [
         {
           rel: 'icon',
