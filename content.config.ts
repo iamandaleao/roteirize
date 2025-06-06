0d8a0b8058afd96c55537587c9dc8cd5882dd46f
Amanda Leão <amandabibiano144@gmail.com>
2025-06-06 18:18:12 -0300

    fix: content source

--- content.config.ts
+++ content.config.ts
@@ -3,34 +3,42 @@ import { asSeoCollection } from '@nuxtjs/seo/content'
 
 export default defineContentConfig({
   collections: {
-    page: defineCollection(asSeoCollection({
-      source: '**/*.md',
-      type: 'page',
-      schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        cover: z.string(),
-        date: z.date(),
-        menu: z.array(z.object({
-          name: z.string(),
-          icon: z.string(),
-          href: z.string(),
-        })).optional(),
-      }),
-    })),
-    blog: defineCollection(asSeoCollection({
-      source: 'blog/**/*.md',
-      type: 'page',
-      schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        region: z.string().optional(),
-        cover: z.string(),
-        thumbnail: z.string(),
-        date: z.date(),
-        published: z.boolean(),
-        tags: z.array(z.string()).optional(),
-      }),
-    })),
+    blog: defineCollection(
+      asSeoCollection({
+        source: "blog/**/*.md",
+        type: "page",
+        schema: z.object({
+          title: z.string(),
+          description: z.string(),
+          region: z.string().optional(),
+          cover: z.string(),
+          thumbnail: z.string(),
+          date: z.date(),
+          published: z.boolean(),
+          tags: z.array(z.string()).optional(),
+        }),
+      })
+    ),
+    page: defineCollection(
+      asSeoCollection({
+        source: "**/*.md",
+        type: "page",
+        schema: z.object({
+          title: z.string(),
+          description: z.string(),
+          cover: z.string(),
+          date: z.date(),
+          menu: z
+            .array(
+              z.object({
+                name: z.string(),
+                icon: z.string(),
+                href: z.string(),
+              })
+            )
+            .optional(),
+        }),
+      })
+    ),
   },
 })
